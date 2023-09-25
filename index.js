c61c7caf0953aeeb34eddd461ff8f20b31b1613b
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-09-25 15:37:57 +0100

    coded block in Js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const grid = document.querySelector('grid')
+const grid = document.querySelector('.grid')
+const blockWidth = 100
+const blockHeight = 20 
+
+
+//create Block
+class Block { 
+    constructor(xAxis, yAxis) { 
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
+}
 
 //draw my block 
-
 function addBlock() {
   const block = document.createElement('div')
   block.classList.add('block')
@@ -9,5 +21,4 @@ function addBlock() {
   block.style.bottom = '50px'
   grid.appendChild(block)
 }
-
 addBlock()
