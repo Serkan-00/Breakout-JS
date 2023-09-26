6708c842c318ce381983f63d4e58b02dacacf5b8
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-09-26 20:19:03 +0100

    test2

--- index.js
+++ index.js
@@ -13,12 +13,21 @@ class Block {
     }
 }
 
-//draw my block 
-function addBlock() {
+//all my blocks 
+const blocks = [
+    new Block(10,270)
+]
+
+console.log(blocks[0])
+
+//draw all my blocks
+function addBlocks() {
+  for (let i = 0; i < blocks.length; i++) {
   const block = document.createElement('div')
   block.classList.add('block')
-  block.style.left = '100px'
+  block.style.left = blocks[i].bottomLeft[0]
   block.style.bottom = '50px'
   grid.appendChild(block)
+  }
 }
-addBlock()
+addBlocks()
