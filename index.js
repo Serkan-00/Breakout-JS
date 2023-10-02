766f6f8d5eac8d43204a746a6dec48d43b87f5ec
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-10-02 15:36:12 +0100

    bounces off right and top

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const blockWidth = 100
 const blockHeight = 20 
 const ballDiameter = 20 
 const boardWidth = 560
+const boardHeight = 300 
 let timerId 
 let xDirection = 2 
 let yDirection = 2 
@@ -118,15 +119,33 @@ timerId = setInterval(moveBall, 30)
 //check for collisions 
 function checkForCollisions() { 
     //check for wall collisions 
-    if (ballCurrentPosition[0] >= (boardWidth - ballDiameter)) { 
+    if (
+    ballCurrentPosition[0] >= (boardWidth - ballDiameter) || 
+    ballCurrentPosition[1] >= (boardHeight - ballDiameter) || 
+    ballCurrentPosition[0] <= 0 
+    ) { 
         changeDirection() 
     }
 }
 
 function changeDirection() { 
     if (xDirection === 2 && yDirection === 2) { 
-        xDirection = -2 
+        yDirection = -2 
         return
+    }
+    if (xDirection == 2 && yDirection === -2) {
+        xDirection = -2
+        return
+    }
+
+    if (xDirection === -2 && yDirection === -2) { 
+        yDirection = 2 
+        return
+    }
+
+    if (xDirection === -2 && yDirection === 2) { 
+        xDirection = 2 
+        return 
     }
 
 }
