55925b39a63e1ef97cd5a3f7451d98a92f993b68
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-10-06 15:28:40 +0100

    blocks dissappear

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const ballDiameter = 20
 const boardWidth = 560
 const boardHeight = 300 
 let timerId 
-let xDirection = 2 
+let xDirection = -2 
 let yDirection = 2 
+let score = 0 
 
 const userStart = [230, 10]
 let currentPosition = userStart
@@ -119,13 +120,29 @@ timerId = setInterval(moveBall, 30)
 
 //check for collisions 
 function checkForCollisions() { 
+    //check for block collisions 
+    for (let i = 0; i < blocks.length; i++) { 
+         if (
+            (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) && 
+            ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1])
+         ) {
+            const allBlocks = Array.from(document.querySelectorAll('.block'))
+            allBlocks[i].classList.remove('block')
+            blocks.splice(i, 1)
+            score++ 
+            scoreDisplay.innerHTML = score 
+         }
+    }
+
+
+
     //check for wall collisions 
     if (
-    ballCurrentPosition[0] >= (boardWidth - ballDiameter) || 
-    ballCurrentPosition[1] >= (boardHeight - ballDiameter) || 
-    ballCurrentPosition[0] <= 0 
-    ) { 
-        changeDirection() 
+      ballCurrentPosition[0] >= (boardWidth - ballDiameter) || 
+      ballCurrentPosition[1] >= (boardHeight - ballDiameter) || 
+      ballCurrentPosition[0] <= 0 
+      ) { 
+      changeDirection() 
     }
 
     //check for game over 
